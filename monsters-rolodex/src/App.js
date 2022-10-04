d1fed15d9b83cb8efd8c46f12d0d6c39c10fea10
Stoyan Gadzhanov <stoyan.gadzhanov@accedia.com>
2022-10-04 13:33:42 +0300

    use setState to have a functionality of having non-static data and also fetched dynamic data from jsonplaceholder

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,43 @@
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(() => {
+        return {
+          monsters: users.map(u => {
+            return {
+              name: u.name,
+              id: u.id
+            }
+          })
+        }
+      },
+        () => console.log(this.state.monsters)
+      ))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.monsters.map(m => <h1 key={m.id}>{m.name}</h1>)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
